c56cba4b093e5703faaa30e2b956b6feda6afbd6
Your Name <your@email.com>
2026-07-22 09:24:03 +0530

    Fix MainActivity context

--- app/src/main/java/com/sa/companion/MainActivity.kt
+++ app/src/main/java/com/sa/companion/MainActivity.kt
@@ -93,7 +93,7 @@ class MainActivity : ComponentActivity() {
 
                 val tasks = TaskParser().parse(command)
 
-                val result = TaskExecutor(this).execute(tasks)
+                val result = TaskExecutor(this@MainActivity).execute(tasks)
 
                 spokenText = """
                 You Said:
